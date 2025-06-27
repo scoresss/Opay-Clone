9fff6da7c2fb8949f2bfa865d6be487023cea4f4
Scores <felixvallejo365@gmail.com>
2025-06-28 00:23:52 +0100

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+// Colors
+const Color kPrimaryColor = Color(0xFF0AA83F); // Opay green
+const Color kSecondaryColor = Color(0xFFEEEEEE);
+const Color kTextColor = Colors.black87;
+const Color kErrorColor = Colors.redAccent;
+
+// Padding
+const double kDefaultPadding = 16.0;
+
+// Border radius
+const double kDefaultRadius = 12.0;
+
+// TextStyles
+const TextStyle kHeadingText = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+  color: kTextColor,
+);
+
+const TextStyle kSubText = TextStyle(
+  fontSize: 14,
+  color: Colors.black54,
+);

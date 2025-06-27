2410bf0afe625c86e2e4c8ddbb2b2725f74cec3f
Scores <felixvallejo365@gmail.com>
2025-06-27 23:19:22 +0100

    Update firestore_service.dart

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,1 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Create user document on registration
+  Future<void> createUser(String uid, String name, String email) async {
+    await _firestore.collection('users').doc(uid).set({
+      'name': name,
+      'email': email,
+      'balance': 2000.0, // Initial balance
+    });
+  }
+
+  // Get user data
+  Stream<DocumentSnapshot> getUserStream(String uid) {
+    return _firestore.collection('users').doc(uid).snapshots();
+  }
+
+  // Update balance
+  Future<void> updateBalance(String uid, double newBalance) async {
+    await _firestore.collection('users').doc(uid).update({
+      'balance': newBalance,
+    });
+  }
+}

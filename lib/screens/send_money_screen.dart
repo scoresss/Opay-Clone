6deb9730852ec7e5bc610df8ab0549b8ce6baed6
Scores <felixvallejo365@gmail.com>
2025-06-27 18:58:19 +0100

    Add files via upload

---
+++ lib/screens/send_money_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SendMoneyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Send Money')),
+    );
+  }
+}
---
+++ lib/screens/transaction_history.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Transaction History')),
+    );
+  }
+}

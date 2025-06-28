cb540aabf21bda8ed6781cbc5de574cde44eb50f
Scores <felixvallejo365@gmail.com>
2025-06-28 16:36:52 +0100

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/opay/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/opay/MainActivity.kt
@@ -2,5 +2,6 @@ package com.example.opay
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    // No override needed for v2
 }

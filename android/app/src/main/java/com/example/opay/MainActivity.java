f060f750f4c768dccee20c659dfd20a60608f9ea
Scores <felixvallejo365@gmail.com>
2025-07-04 17:47:49 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/example/opay/MainActivity.java
+++ android/app/src/main/java/com/example/opay/MainActivity.java
@@ -2,5 +2,6 @@ package com.example.opay;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterActivity 
+{
 }
